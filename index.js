f4a481562c519cf2653048fe6528361f12b88af4
Asmaa Kishk <asmaakishk@gmail.com>
2023-06-02 23:01:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+//document.getElementById("count-el").innerHTML = 66666666666666
+let count = 10023;
+console.log(count)
+let myAge = 29
+console.log(myAge)
---
+++ train.js
@@ -0,0 +1,14 @@
+var sum = 2 + 5;
+//console.log(sum)
+var name = "ASMAA"
+//alert(name)
+var sun = 22 - 6;
+//alert(sun)
+var product = 6 * 4;
+
+var myVar = 88;
+myVar++;
+alert(myVar)
+
+var sat = 'I am "kishk" \n mohammed \t nessma \bsanaa \\moo \f asmaa \f ali '
+console.log(sat)
